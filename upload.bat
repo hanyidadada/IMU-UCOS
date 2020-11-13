827025b3f71b78c027a8a7c112eae2a29a192bbd
hanyidadada <44640043+hanyidadada@users.noreply.github.com>
2020-11-13 21:55:59 +0800

    第三章课后题

--- upload.bat
+++ upload.bat
@@ -4,6 +4,6 @@ git add .
 ::第一次提交
 git commit -m "第三章课后题"
 ::同步远程仓库
-git pull origin master:master --allow-unrelated-histories
+git pull origin master --allow-unrelated-histories
 ::推送本地分支到远程
-git push -u origin master:master
+git push -u origin master
